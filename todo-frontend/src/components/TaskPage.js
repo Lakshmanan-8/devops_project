74c6a42767b634cdd3bb142fd970366d233a4256
lakshmanan-8 <lakshukrishna3@gmail.com>
2025-06-20 23:32:58 +0530

    color green added

--- todo-frontend/src/components/TaskPage.js
+++ todo-frontend/src/components/TaskPage.js
@@ -24,7 +24,7 @@ export default function TaskPage()
         <div>
             {!showform && !showlist &&(
                 <>
-                <h3 style={{cursor:'pointer',color:'red'}}onClick={handleonclickadd}><a>New Task</a></h3>
+                <h3 style={{cursor:'pointer',color:'green'}}onClick={handleonclickadd}><a>New Task</a></h3>
                 <h3 style={{cursor:'pointer',color:'red'}}onClick={handleonclicklist}><a>All Task</a></h3>
 
                 </>
